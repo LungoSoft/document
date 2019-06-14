81facf136cee1d3902f056d39204904c71647f52
Luis Guillen <luisguillen200592@gmail.com>
2019-06-14 14:35:12 -0500

    Contemplar todas las líneas independientemente de que sea de la misma cabecera

--- tests/DocumentManagerTest.php
+++ tests/DocumentManagerTest.php
@@ -17,12 +17,14 @@ class DocumentManagerTest extends TestCase
         $docModel1 = new Model;
         $docModel11 = new Model;
         $docModel2 = new Model;
+        $docModel21 = new Model;
 
         $docModel1->status = 'Abierto';
         $docModel1->quantity = 5;
         $docModel11->status = 'Parcialmente Abierto';
         $docModel11->quantity = 5;
-        $docModel2->quantity = 3;
+        $docModel2->quantity = 2;
+        $docModel21->quantity = 1;
 
         $mockDoc1->shouldReceive('getModels')->andReturn(false);
         $mockDoc1->shouldReceive('getModel')->withArgs([1])->andReturn($docModel1);
@@ -33,7 +35,7 @@ class DocumentManagerTest extends TestCase
         $mockDoc1->shouldReceive('getLinePartiallyCloseStatus')->andReturn('Parcialmente Abierto');
         $mockDoc1->shouldReceive('getLineQuantityColumn')->andReturn('quantity');
 
-        $mockDoc2->shouldReceive('getModelLineByFK')->andReturn($docModel2);
+        $mockDoc2->shouldReceive('getModelLinesByFK')->andReturn(collect([$docModel2, $docModel21]));
         $mockDoc2->shouldReceive('getLineColumnStatus')->andReturn('status');
         $mockDoc2->shouldReceive('getLineCloseStatus')->andReturn('Cerrado');
         $mockDoc2->shouldReceive('getLineQuantityColumn')->andReturn('quantity');
